cd8f3bf0b0641898af7889947206ee46f145b293
David Souther <davidsouther@gmail.com>
2012-06-06 22:18:02 -0400

    Lots easier. REQUIRES! docco and pygmentize.

--- tasks/docco.js
+++ tasks/docco.js
@@ -6,44 +6,16 @@
 
 module.exports = function(grunt) {
 
-	// Please see the grunt documentation for more information regarding task and
-	// helper creation: https://github.com/cowboy/grunt/blob/master/docs/toc.md
-
-	// ==========================================================================
-	// TASKS
-	// ==========================================================================
-
+	// ### TASKS
 	grunt.registerMultiTask('docco', 'Docco processor.', function() {
-		var files = grunt.file.expandFiles(this.file.src);
-		grunt.log.writeln('Doccoing ' + files.join(' '));
-		grunt.helper("docco", files, {
-			done: function(){
-				grunt.log.writeln("Docco'd " + files.join(' '));
-			}
-		});
-	});
-
-	// ==========================================================================
-	// HELPERS
-	// ==========================================================================
-
-	grunt.registerHelper('docco', function(paths, options) {
-		return grunt.utils.spawn({
-			cmd: 'docco',
-			args: paths
+		var done = this.async();
+		var src = grunt.file.expandFiles(this.file.src);
+		grunt.utils.spawn({
+			cmd: "docco",
+			args: src
 		}, function(err, result, code){
-			if (!err) { return options.done(null); }
-			// Something went horribly wrong.
-			grunt.verbose.or.writeln();
-			grunt.log.write('Running Docco...').error();
-			if (code === 127) {
-				grunt.warn('Docco not found.', options.code);
-			} else {
-				result.split('\n').forEach(grunt.log.error, grunt.log);
-				grunt.warn('Docco exited unexpectedly with exit code ' + code + '.', options.code);
-			}
-			options.done(code);
+			grunt.log.writeln("Doccoed [" + src.join(", ") + "]; " + err ? err : "(No errors)" + "\n" + result + " " + code);
+			done();
 		});
 	});
-
 };
