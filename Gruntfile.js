9b5f3cd1a7cfbe388b957bd103562985af5cf916
David Souther <davidsouther@gmail.com>
2013-09-30 10:40:39 -0400

    JSHinted

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,33 +1,43 @@
 module.exports = function(grunt) {
 
-  // Project configuration.
   grunt.initConfig({
     clean: { tests: ["docs"] },
+
+    jshint: {
+      grunt: ['Gruntfile.js'],
+      tasks: ['tasks/docco.js'],
+      tests: ['test/*.js'],
+      options: {
+        node: true
+      }
+    },
+
     docco: {
       tests: {
         src: ['test/**/*.js', 'test/**/*.coffee'],
         dest: "docs/"
       },
       'custom-css-test': {
-          src: ['test/**/*.js'],
-          dest: 'docs/',
-          options: {
-              css: 'test/fixtures/custom.css'
-          }
+        src: ['test/**/*.js'],
+        dest: 'docs/',
+        options: {
+            css: 'test/fixtures/custom.css'
+        }
       }
     },
+
     nodeunit: {
       tests: ['test/*_test.js']
     }
   });
 
-  // Load local tasks.
   grunt.loadTasks('tasks');
+
   grunt.loadNpmTasks('grunt-contrib-nodeunit');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-clean');
+
   grunt.registerTask('test', ['clean:tests', 'docco', 'nodeunit:tests']);
-
-  // Default task.
+  grunt.registerTask('lint', ['jshint:grunt', 'jshint:tasks', 'jshint:tests']);
   grunt.registerTask('default', ['lint', 'docco']);
-
 };
